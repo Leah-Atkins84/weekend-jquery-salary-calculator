a6f553bbe6a927c8176a6b8bad6b43c728653f05
Leah Atkins <leah.atkins84@gmail.com>
2022-01-30 15:19:11 -0600

    This will add functions

--- client.js
+++ client.js
@@ -1,17 +1,29 @@
-console.log('Salary Calculator');
+// console.log('Salary Calculator');
 $(document).ready(readyNow);
+
+
 
 function readyNow() {
     console.log('ready now');
-    $('#submit-button').on('click', addEmp);
+    $('.submit-button').on('click', addEmp);
     //$('.clear-list').on('click', clearEmp);
     
 };
 
 function addEmp() {
-    console.log('in add emp');
+    console.log('Added:', $( '#fName' ).val());
+   
+    $('#employee-table').append('<tr><td' + $('#fName') + '<tr><td' + $('#lName') + '<tr><td>' + $('#eId') + '<tr><td>' + $('#eTitle') + '<tr><td>' + $('#aSalary'));
     
+    $('#fName').val(''), $('#lName').val(''), $('#eId').val(''), $('#eTitle').val(''), $('#aSalary');
 };
+
+//$('#fName').val(),$('#lName').val(), $('#eId').val(), $('#eTitle').val(), $('#aSalary').val();
+
+function empTableDisplay() {
+    console.log('in empTableDisplay');
+    
+}
 
 
 
@@ -20,3 +32,18 @@ function addEmp() {
 // function clearEmp() {
     
 // }
+
+
+
+// function newEmployee(firstName, lastName, empId, empTitle, anSalary){
+//     console.log('in newEmployee function', firstName, lastName, empId, empTitle, anSalary);
+//     let employeeObj={
+//         first: firstName,
+//         last: lastName,
+//         id: empId,
+//         title: empTitle,
+//         salary: anSalary 
+//     }
+//     employees.push(employeeObj);// pushes employee info into employee array when employee added
+//     return true;
+// }// end newEmployee
