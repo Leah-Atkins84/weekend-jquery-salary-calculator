88c66f44e34f028dfc08792cd475a8947d4dbfd6
Leah Atkins <leah.atkins84@gmail.com>
2022-01-29 20:29:36 -0600

    This will give html ids

--- client.js
+++ client.js
@@ -2,5 +2,19 @@ console.log('Salary Calculator');
 $(document).ready(readyNow);
 
 function readyNow() {
+    $('#addButton').on('click', addEmp);
+    //$('.clear-list').on('click', clearEmp);
     
 };
+
+function addEmp() {
+    
+}
+
+
+
+
+
+// function clearEmp() {
+    
+// }
