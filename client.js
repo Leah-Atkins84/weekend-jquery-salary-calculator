dd64710a2f2244f488bac7660e96c3cad381c884
Leah Atkins <leah.atkins84@gmail.com>
2022-01-30 18:21:52 -0600

    This will make employee info add to table

--- client.js
+++ client.js
@@ -5,35 +5,52 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('ready now');
-    $('.submit-button').on('click', addEmp);
+   //$('.submit-button').on('click', addEmp);
+    $('.submit-button').on('click', newEmployee);
+
     //$('.clear-list').on('click', clearEmp);
     
 };
 
-let employees =[];
+let employees=[];
 
-function newEmployee(firstName, lastName, empId, empTitle, anSalary){
-    console.log('in newEmployee function', fName, lName, eId, eTitle, aSalary);
+function newEmployee(){
+    // console.log('in newEmployee function', fName, lName, eId, eTitle, aSalary);
     let employeeObj={
-        first: ftName,
-        last: lName,
-        id: eId,
-        title: eTitle,
-        salary: aSalary 
+        first: $('#fName').val(),
+        last: $('#lName').val(),
+        id: $('#eId').val(),
+        title: $('#eTitle').val(),
+        salary: $('#aSalary').val() 
     }
     employees.push(employeeObj);// pushes employee info into employee array when employee added
+   //newEmployee(); 
+   $('#employee-table').append(
+       `<tr> 
+       <td> ${employeeObj.first}</td> 
+       <td> ${employeeObj.last}</td>
+       <td> ${employeeObj.id}</td>
+       <td> ${employeeObj.title}</td>
+       <td> ${employeeObj.salary}</td>
+       </tr>`);
+       $('#fName').val(''), $('#lName').val(''), $('#eId').val(''), $('#eTitle').val(''), $('#aSalary').val('');
     
+
+``
 };// end newEmployee
 
-function addEmp() {
-    console.log('Added:', $( '#fName' ).val());
+// function addEmp() {
+//     console.log('Added:', $( '#fName' ).val());
     
-    $('#fName').val(),$('#lName').val(), $('#eId').val(), $('#eTitle').val(), $('#aSalary').val();
+//     //$('#fName').val(), $('#lName').val(), $('#eId').val(), $('#eTitle').val(), $('#aSalary').val();
    
-    $('#employee-table').append('<tr>', '<td>' + `${employees.fName}` + '</td>', '<td>' + `${employees.lName}` + '</td>', '<td>' + `${employees.eId}` + '</td>', '</td>' + `${employees.eTitle}` + '</td>', '<td>' + `${employees.aSalary}` + '</td>', '</tr>');
+//$('#employee-table').append(`'<tr>', '<td>' + ${employees.fName} + '</td>', '<td>' + ${employees.lName} + '</td>', '<td>' + ${employees.eId} + '</td>', '</td>' + ${employees.eTitle} + '</td>', '<td>' + ${employees.aSalary} + '</td>', '</tr>'`);
     
-    $('#fName').val(''), $('#lName').val(''), $('#eId').val(''), $('#eTitle').val(''), $('#aSalary').val('');
-};
+//    // $('#fName').val(''), $('#lName').val(''), $('#eId').val(''), $('#eTitle').val(''), $('#aSalary').val('');
+    
+// };
+
+console.log(employees);
 
 
 
@@ -54,16 +71,3 @@ function empTableDisplay() {
 
 
 
-
-// function newEmployee(firstName, lastName, empId, empTitle, anSalary){
-//     console.log('in newEmployee function', firstName, lastName, empId, empTitle, anSalary);
-//     let employeeObj={
-//         first: firstName,
-//         last: lastName,
-//         id: empId,
-//         title: empTitle,
-//         salary: anSalary 
-//     }
-//     employees.push(employeeObj);// pushes employee info into employee array when employee added
-//     return true;
-// }// end newEmployee
